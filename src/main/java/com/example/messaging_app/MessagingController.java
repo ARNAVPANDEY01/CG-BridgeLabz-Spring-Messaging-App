7c16cf244b91f26801b219c2acef6a53908ea977
Arnav Pandey <pandeyarnav2002@gmail.com>
2025-02-25 15:59:22 +0530

    UC-2_Messaging_App Task Done

--- src/main/java/com/example/messaging_app/MessagingController.java
+++ src/main/java/com/example/messaging_app/MessagingController.java
@@ -2,14 +2,27 @@ package com.example.messaging_app;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-//UC-1
+
 @RestController
 public class MessagingController {
-    @GetMapping("/hello")
-    public String sayHello(){
-        return "Hello from BridgeLabz";
+
+
+    //UC1: Handle GET Request to return Hello message
+    //@GetMapping("/hello")
+    //public String sayHello(){
+
+    //    return "Hello from BridgeLabz";
+    //}
+
+
+
+    //UC2: GET REQUEST with query paramenter
+    @GetMapping
+    public String name(@RequestParam String name){
+        return "Hello" + name + "!";
     }
 
 }
